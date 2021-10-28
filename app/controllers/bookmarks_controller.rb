8a5e99c09fb2c2b56ee84afbe01bd2e89ebbc082
Christian Bongard <christian.bongard90@gmail.com>
2021-10-28 16:56:55 +0400

    adding movie functionality

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,24 @@
 class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    if @bookmark.save
+      flash[:success] = "Object successfully created"
+    else
+      flash[:error] = "Something went wrong"
+    end
+    redirect_to list_path(params[:list_id])
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:movie_id, :comment)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "lists#index"
-  resources :movies
-  resources :lists
+  resources :lists do
+    resources :bookmarks, only: %i[new create show destory]
+  end
 end
